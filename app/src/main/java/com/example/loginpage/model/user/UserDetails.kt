56b32beb30401e440278da7dca744c51594fe2e2
kalamsheik80 <sheikkalam80@gmail.com>
2025-02-13 12:42:06 +0530

    New code push on the 13/02/2025

--- app/src/main/java/com/example/loginpage/model/user/UserDetails.kt
+++ app/src/main/java/com/example/loginpage/model/user/UserDetails.kt
@@ -1,6 +1,6 @@
 package com.example.loginpage.model.user
 
-data class Kolagaram(
+data class UserDetails(
     val name: String? = null,
     val turnover: String? = null,
     val gpSanctionId: String? = null,
